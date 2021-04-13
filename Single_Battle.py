5f630dd9fdc1a30699744f8c637df34e9a529f05
stcarda <seancarda@msn.com>
2021-04-12 18:20:28 -0700

    Added lines to Single_Battle. __init__ needs changing.

--- Single_Battle.py
+++ Single_Battle.py
@@ -6,7 +6,7 @@ def __init__(self):
     self.playerItems = 0
 
 
-def createTeam():
+def createTeam(self):
     print("\n*** Welcome to Single Battles! ***")
     print("----------------------------------\n")
     print("How many Pokemon would you like in your team?")
@@ -15,22 +15,42 @@ def createTeam():
         if team_size == 0:
             print("\nSorry, but you cannot have a team of zero!\n")
         else:
-            print("\nSorry, teams other than one are unsupported right now. Please choose a ")
+            print("\nSorry, teams other than one are unsupported right now. Please choose a ") 
             print("smaller team size.\n")
         print("How many Pokemon would you like in your team?")
         team_size = int(input(": "))
+    self.playerTeam = [None] * team_size
+    self.enemyTeam = [None] * team_size
     print("\nWould like like randomized Pokemon? (y/n)")
     randomized = input(": ").lower()
     if randomized == "n":
-        assignRandomPokemon()
-
+        assignRandomPokemon(playerTeam)
+        assignRandomPokemon(enemyTeam)
+    else:
+        choosePokemon()
     print(randomized)
 
 def choosePokemon():
-    print("Temp")
+    i = 1
+    for pokemon in playerTeam:
+        print("Please select Pokemon " + i + " ")
+        name = input(": ")
+        pokemon = eval(name)
+        print("DEBUG " + pokemon.name)
+        i += 1
+        
 
-def assignRandomPokemon():
-    
+
+# This method accepts no inputs and assigns random Pokemon for both teams. 
+# This method needs heavy edits. I would like this method to assign random pokemon based on how 
+# many Pokemon I have included in the pokemon_database folder.
+def assignRandomPokemon(team):
+    for pokemon in team:
+        chance = random.randint(0, 1)
+        if chance == 1:
+            pokemon = Charmander()
+        else:
+            pokemon = Bulbasaur()
 
 def showPokemonList():
     print("Temp")
@@ -46,4 +66,3 @@ def showMoves(target):
     for move in move_list:
         print(move)
 
-createTeam()
